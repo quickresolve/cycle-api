10dc664de8dd67be164081ed9a6aa215fb518fae
psiclops <nicholasrusso@icloud.com>
2016-05-13 10:48:49 -0700

    Initial api

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+CycleApi::Application.config.secret_token = 'd0c84ed518f05c183d0f7a372abc5a3c68499e9d7d581151a973155bd4496227b910bdb154b394f1db687b76499349dd8fbb747496556a286344ef296cdd21ee'
