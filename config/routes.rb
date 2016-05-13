549c910c33230abb4d1766d0c30f0dd557f8fef5
psiclops <nicholasrusso@icloud.com>
2016-05-13 11:40:12 -0700

    Undo nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
   namespace :api do
-    resources :locations, only: [:show] do
-      resources :landmarks, only: [:show]
-    end
+    resources :locations, only: [:show]
+    resources :landmarks, only: [:show]
   end
 
 end
