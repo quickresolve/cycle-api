78fdc37d4cf9364d76f274a650c14dfdff369207
psiclops <nicholasrusso@icloud.com>
2016-05-13 15:07:46 -0700

    Update nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   namespace :api do
-    resources :locations, only: [:show]
-    resources :landmarks, only: [:index]
+    resources :locations, only: [:show] do
+      resources :landmarks, only: [:index]
+    end
   end
 
 end
