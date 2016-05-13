77bc1762a81faf1becce425431db62bea6d21151
psiclops <nicholasrusso@icloud.com>
2016-05-13 11:58:13 -0700

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :locations, only: [:show]
-    resources :landmarks, only: [:show]
+    resources :landmarks, only: [:index]
   end
 
 end
