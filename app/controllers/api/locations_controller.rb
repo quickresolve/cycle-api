098bc47cdb187ae564efc3e7c43dd39e8ce8d20a
psiclops <nicholasrusso@icloud.com>
2016-05-14 14:37:42 -0700

    Add index route for locations

--- app/controllers/api/locations_controller.rb
+++ app/controllers/api/locations_controller.rb
@@ -1,5 +1,10 @@
 class Api::LocationsController < ApplicationController
 
+  def index
+    @locations = Location.all
+    render json: @locations
+  end
+
   def show
     @location = Location.find(params[:id])
     render json: @location
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api do
-    resources :locations, only: [:show] do
+    resources :locations, only: [:index, :show] do
       resources :landmarks, only: [:index]
     end
   end
