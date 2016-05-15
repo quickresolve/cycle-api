31eb4b04beea9dff1a5497e4733de926a3480936
psiclops <nicholasrusso@icloud.com>
2016-05-14 17:31:14 -0700

    Add some of coords to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ Location.create(
                 distance: 11.0,
                 elevation_up: 230,
                 elevation_down: 397,
-                terrain: "crapi"
+                terrain: "Paved"
 )
 Location.create(
                 title: "San Francisco Southern Waterfront",
@@ -18,13 +18,13 @@ Location.create(
                 image_url: "url",
                 map_url: "url",
                 start_lat: 37.7786,
-                start_long: 122.5048,
+                start_long: 122.3893,
                 end_lat: 37.6808,
                 end_long: 122.4000,
                 distance: 8.0,
                 elevation_up: 407,
                 elevation_down: 292,
-                terrain: "crapi"
+                terrain: "Mostly Paved"
 )
 Location.create(
                 title: "Brisbane Lagoon to Bayside Park",
@@ -38,7 +38,7 @@ Location.create(
                 distance: 10.9,
                 elevation_up: 115,
                 elevation_down: 223,
-                terrain: "crapi"
+                terrain: "Mostly Paved"
 )
 Location.create(
                 title: "Anza Lagoon to Belmont Slough",
@@ -49,10 +49,10 @@ Location.create(
                 start_long: 122.3448,
                 end_lat: 37.5364,
                 end_long: 122.2455,
-                distance: 10.9,
-                elevation_up: 115,
-                elevation_down: 223,
-                terrain: "crapi"
+                distance: 9.3,
+                elevation_up: 0,
+                elevation_down: 0,
+                terrain: "Paved"
 )
 Location.create(
                 title: "Belmont Slough to Bedwell Bayfront Park",
@@ -63,9 +63,275 @@ Location.create(
                 start_long: 122.2455,
                 end_lat: 37.4906,
                 end_long: 122.1772,
-                distance: 10.9,
-                elevation_up: 115,
-                elevation_down: 223,
+                distance: 9.2,
+                elevation_up: 0,
+                elevation_down: 0,
+                terrain: "Mostly Paved"
+)
+Location.create(
+                title: "Bedwell Bayfront Park to Alviso",
+                desc: "description",
+                image_url: "url",
+                map_url: "url",
+                start_lat: 37.4906,
+                start_long: 122.1772,
+                end_lat: 37.4296,
+                end_long: 121.9792,
+                distance: 10,
+                elevation_up: 1,
+                elevation_down: 2,
+                terrain: "crapi"
+)
+Location.create(
+                title: "Alviso to Newark",
+                desc: "description",
+                image_url: "url",
+                map_url: "url",
+                start_lat: 37.4296,
+                start_long: 121.9792,
+                end_lat: 37.5256,
+                end_long: 122.0715,
+                distance: 10,
+                elevation_up: 1,
+                elevation_down: 2,
+                terrain: "crapi"
+)
+Location.create(
+                title: "Newark to San Leandro",
+                desc: "description",
+                image_url: "url",
+                map_url: "url",
+                start_lat: 37.5256,
+                start_long: 122.0715,
+                end_lat: 37.7249,
+                end_long: 122.1561,
+                distance: 10,
+                elevation_up: 1,
+                elevation_down: 2,
+                terrain: "crapi"
+)
+Location.create(
+                title: "San Leandro to Bay Farm Island",
+                desc: "description",
+                image_url: "url",
+                map_url: "url",
+                start_lat: 37.7249,
+                start_long: 122.1561,
+                end_lat: 37.7374,
+                end_long: 122.2370,
+                distance: 10,
+                elevation_up: 1,
+                elevation_down: 2,
+                terrain: "crapi"
+)
+Location.create(
+                title: "Alameda & Oakland",
+                desc: "description",
+                image_url: "url",
+                map_url: "url",
+                # start_lat: 37.,
+                # start_long: 122.,
+                # end_lat: 37.,
+                # end_long: 122.,
+                distance: 10,
+                elevation_up: 1,
+                elevation_down: 2,
+                terrain: "crapi"
+)
+Location.create(
+                title: "SF-Oakland Bay Bridge",
+                desc: "description",
+                image_url: "url",
+                map_url: "url",
+                # start_lat: 37.,
+                # start_long: 122.,
+                # end_lat: 37.,
+                # end_long: 122.,
+                distance: 10,
+                elevation_up: 1,
+                elevation_down: 2,
+                terrain: "crapi"
+)
+Location.create(
+                title: "Oakland to Albany",
+                desc: "description",
+                image_url: "url",
+                map_url: "url",
+                # start_lat: 37.,
+                # start_long: 122.,
+                # end_lat: 37.,
+                # end_long: 122.,
+                distance: 10,
+                elevation_up: 1,
+                elevation_down: 2,
+                terrain: "crapi"
+)
+Location.create(
+                title: "Albany to Richmond",
+                desc: "description",
+                image_url: "url",
+                map_url: "url",
+                # start_lat: 37.,
+                # start_long: 122.,
+                # end_lat: 37.,
+                # end_long: 122.,
+                distance: 10,
+                elevation_up: 1,
+                elevation_down: 2,
+                terrain: "crapi"
+)
+Location.create(
+                title: "Pt. San Pablo to Pt. Pinole",
+                desc: "description",
+                image_url: "url",
+                map_url: "url",
+                # start_lat: 37.,
+                # start_long: 122.,
+                # end_lat: 37.,
+                # end_long: 122.,
+                distance: 10,
+                elevation_up: 1,
+                elevation_down: 2,
+                terrain: "crapi"
+)
+Location.create(
+                title: "Point Wilson to Carquinez Bridge",
+                desc: "description",
+                image_url: "url",
+                map_url: "url",
+                # start_lat: 37.,
+                # start_long: 122.,
+                # end_lat: 37.,
+                # end_long: 122.,
+                distance: 10,
+                elevation_up: 1,
+                elevation_down: 2,
+                terrain: "crapi"
+)
+Location.create(
+                title: "Carquinez Strait",
+                desc: "description",
+                image_url: "url",
+                map_url: "url",
+                # start_lat: 37.,
+                # start_long: 122.,
+                # end_lat: 37.,
+                # end_long: 122.,
+                distance: 10,
+                elevation_up: 1,
+                elevation_down: 2,
+                terrain: "crapi"
+)
+Location.create(
+                title: "Benicia State Recreation Area to White Slough Path",
+                desc: "description",
+                image_url: "url",
+                map_url: "url",
+                # start_lat: 37.,
+                # start_long: 122.,
+                # end_lat: 37.,
+                # end_long: 122.,
+                distance: 10,
+                elevation_up: 1,
+                elevation_down: 2,
+                terrain: "crapi"
+)
+Location.create(
+                title: "Northern Vallejo to Napa",
+                desc: "description",
+                image_url: "url",
+                map_url: "url",
+                # start_lat: 37.,
+                # start_long: 122.,
+                # end_lat: 37.,
+                # end_long: 122.,
+                distance: 10,
+                elevation_up: 1,
+                elevation_down: 2,
+                terrain: "crapi"
+)
+Location.create(
+                title: "Hudeman Slough to Black Point",
+                desc: "description",
+                image_url: "url",
+                map_url: "url",
+                # start_lat: 37.,
+                # start_long: 122.,
+                # end_lat: 37.,
+                # end_long: 122.,
+                distance: 10,
+                elevation_up: 1,
+                elevation_down: 2,
+                terrain: "crapi"
+)
+Location.create(
+                title: "Petaluma River to McInnis Park",
+                desc: "description",
+                image_url: "url",
+                map_url: "url",
+                # start_lat: 37.,
+                # start_long: 122.,
+                # end_lat: 37.,
+                # end_long: 122.,
+                distance: 10,
+                elevation_up: 1,
+                elevation_down: 2,
+                terrain: "crapi"
+)
+Location.create(
+                title: "Las Gallinas Valley Sanitary District to Point San Pedro Road",
+                desc: "description",
+                image_url: "url",
+                map_url: "url",
+                # start_lat: 37.,
+                # start_long: 122.,
+                # end_lat: 37.,
+                # end_long: 122.,
+                distance: 10,
+                elevation_up: 1,
+                elevation_down: 2,
+                terrain: "crapi"
+)
+Location.create(
+                title: "Point San Pedro Road to Paradise Drive",
+                desc: "description",
+                image_url: "url",
+                map_url: "url",
+                # start_lat: 37.,
+                # start_long: 122.,
+                # end_lat: 37.,
+                # end_long: 122.,
+                distance: 10,
+                elevation_up: 1,
+                elevation_down: 2,
+                terrain: "crapi"
+)
+Location.create(
+                title: "Paradise Drive to Bothin Marsh",
+                desc: "description",
+                image_url: "url",
+                map_url: "url",
+                # start_lat: 37.,
+                # start_long: 122.,
+                # end_lat: 37.,
+                # end_long: 122.,
+                distance: 10,
+                elevation_up: 1,
+                elevation_down: 2,
+                terrain: "crapi"
+)
+Location.create(
+                title: "Strawberry Drive to Golden Gate Bridge",
+                desc: "description",
+                image_url: "url",
+                map_url: "url",
+                # start_lat: 37.,
+                # start_long: 122.,
+                # end_lat: 37.,
+                # end_long: 122.,
+                distance: 10,
+                elevation_up: 1,
+                elevation_down: 2,
                 terrain: "crapi"
 )
 
