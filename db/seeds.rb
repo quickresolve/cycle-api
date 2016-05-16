c69bdbc2887cb9cabac62fef9205a78bd9c62f99
psiclops <nicholasrusso@icloud.com>
2016-05-15 22:26:43 -0700

    Finish seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -131,213 +131,344 @@ Location.create(
                 map_url: "url",
                 start_lat: 37.7471,
                 start_long: 122.2306,
-                # end_lat: 37.,
-                # end_long: 122.,
-                distance: 10,
-                elevation_up: 1,
-                elevation_down: 2,
-                terrain: "crapi"
+                end_lat: 37.8052,
+                end_long: 122.3230,
+                distance: 9,
+                elevation_up: 0,
+                elevation_down: 0,
+                terrain: "Mostly Paved"
 )
 Location.create(
                 title: "SF-Oakland Bay Bridge",
                 desc: "description",
                 image_url: "url",
                 map_url: "url",
-                # start_lat: 37.,
-                # start_long: 122.,
-                # end_lat: 37.,
-                # end_long: 122.,
-                distance: 10,
+                start_lat: 37.8052,
+                start_long: 122.3230,
+                end_lat: 37.7956,
+                end_long: 122.3934,
+                distance: 9.4,
                 elevation_up: 1,
                 elevation_down: 2,
-                terrain: "crapi"
+                terrain: "Mostly Paved"
 )
 Location.create(
                 title: "Oakland to Albany",
                 desc: "description",
                 image_url: "url",
                 map_url: "url",
-                # start_lat: 37.,
-                # start_long: 122.,
-                # end_lat: 37.,
-                # end_long: 122.,
-                distance: 10,
-                elevation_up: 1,
-                elevation_down: 2,
-                terrain: "crapi"
+                start_lat: 37.8052,
+                start_long: 122.3230,
+                end_lat: 37.9009,
+                end_long: 122.3247,
+                distance: 10.2,
+                elevation_up: 0,
+                elevation_down: 0,
+                terrain: "Mostly Paved"
 )
 Location.create(
                 title: "Albany to Richmond",
                 desc: "description",
                 image_url: "url",
                 map_url: "url",
-                # start_lat: 37.,
-                # start_long: 122.,
-                # end_lat: 37.,
-                # end_long: 122.,
-                distance: 10,
-                elevation_up: 1,
-                elevation_down: 2,
-                terrain: "crapi"
+                start_lat: 37.9009,
+                start_long: 122.3247,
+                end_lat: 37.9640,
+                end_long: 122.4276,
+                distance: 9.9,
+                elevation_up: 269,
+                elevation_down: 256,
+                terrain: "Mostly Paved"
 )
 Location.create(
                 title: "Pt. San Pablo to Pt. Pinole",
                 desc: "description",
                 image_url: "url",
                 map_url: "url",
-                # start_lat: 37.,
-                # start_long: 122.,
-                # end_lat: 37.,
-                # end_long: 122.,
-                distance: 10,
-                elevation_up: 1,
-                elevation_down: 2,
-                terrain: "crapi"
+                start_lat: 37.9640,
+                start_long: 122.4276,
+                end_lat: 37.0001,
+                end_long: 122.3546,
+                distance: 10.9,
+                elevation_up: 348,
+                elevation_down: 315,
+                terrain: "Mostly Paved"
 )
 Location.create(
                 title: "Point Wilson to Carquinez Bridge",
                 desc: "description",
                 image_url: "url",
                 map_url: "url",
-                # start_lat: 37.,
-                # start_long: 122.,
-                # end_lat: 37.,
-                # end_long: 122.,
-                distance: 10,
-                elevation_up: 1,
-                elevation_down: 2,
-                terrain: "crapi"
+                start_lat: 37.0001,
+                start_long: 122.3546,
+                end_lat: 37.0566,
+                end_long: 122.2282,
+                distance: 8.2,
+                elevation_up: 420,
+                elevation_down: 476,
+                terrain: "Mostly Paved"
 )
 Location.create(
                 title: "Carquinez Strait",
                 desc: "description",
                 image_url: "url",
                 map_url: "url",
-                # start_lat: 37.,
-                # start_long: 122.,
-                # end_lat: 37.,
-                # end_long: 122.,
-                distance: 10,
-                elevation_up: 1,
-                elevation_down: 2,
-                terrain: "crapi"
+                start_lat: 37.0225,
+                start_long: 122.1420,
+                end_lat: 37.0733,
+                end_long: 122.1926,
+                distance: 8.1,
+                elevation_up: 302,
+                elevation_down: 302,
+                terrain: "Mostly Paved"
 )
 Location.create(
                 title: "Benicia State Recreation Area to White Slough Path",
                 desc: "description",
                 image_url: "url",
                 map_url: "url",
-                # start_lat: 37.,
-                # start_long: 122.,
-                # end_lat: 37.,
-                # end_long: 122.,
-                distance: 10,
-                elevation_up: 1,
-                elevation_down: 2,
-                terrain: "crapi"
+                start_lat: 37.0733,
+                start_long: 122.1926,
+                end_lat: 37.1152,
+                end_long: 122.2718,
+                distance: 6.8,
+                elevation_up: 203,
+                elevation_down: 203,
+                terrain: "Mostly Paved"
 )
 Location.create(
                 title: "Northern Vallejo to Napa",
                 desc: "description",
                 image_url: "url",
                 map_url: "url",
-                # start_lat: 37.,
-                # start_long: 122.,
-                # end_lat: 37.,
-                # end_long: 122.,
-                distance: 10,
-                elevation_up: 1,
-                elevation_down: 2,
-                terrain: "crapi"
+                start_lat: 37.1152,
+                start_long: 122.2718,
+                end_lat: 37.3073,
+                end_long: 122.2697,
+                distance: 13.6,
+                elevation_up: 217,
+                elevation_down: 207,
+                terrain: "Mostly Paved"
 )
 Location.create(
                 title: "Hudeman Slough to Black Point",
                 desc: "description",
                 image_url: "url",
                 map_url: "url",
-                # start_lat: 37.,
-                # start_long: 122.,
-                # end_lat: 37.,
-                # end_long: 122.,
-                distance: 10,
-                elevation_up: 1,
-                elevation_down: 2,
-                terrain: "crapi"
+                start_lat: 37.2063,
+                start_long: 122.3723,
+                end_lat: 37.1149,
+                end_long: 122.5175,
+                distance: 36.7,
+                elevation_up: 1050,
+                elevation_down: 860,
+                terrain: "Mostly Paved"
 )
 Location.create(
                 title: "Petaluma River to McInnis Park",
                 desc: "description",
                 image_url: "url",
                 map_url: "url",
-                # start_lat: 37.,
-                # start_long: 122.,
-                # end_lat: 37.,
-                # end_long: 122.,
-                distance: 10,
-                elevation_up: 1,
-                elevation_down: 2,
-                terrain: "crapi"
+                start_lat: 37.1149,
+                start_long: 122.5175,
+                end_lat: 37.0234,
+                end_long: 122.5104,
+                distance: 13.8,
+                elevation_up: 233,
+                elevation_down: 417,
+                terrain: "Mostly Paved"
 )
 Location.create(
                 title: "Las Gallinas Valley Sanitary District to Point San Pedro Road",
                 desc: "description",
                 image_url: "url",
                 map_url: "url",
-                # start_lat: 37.,
-                # start_long: 122.,
-                # end_lat: 37.,
-                # end_long: 122.,
-                distance: 10,
-                elevation_up: 1,
-                elevation_down: 2,
-                terrain: "crapi"
+                start_lat: 37.0234,
+                start_long: 122.5104,
+                end_lat: 37.9732,
+                end_long: 122.4837,
+                distance: 8.4,
+                elevation_up: 272,
+                elevation_down: 276,
+                terrain: "Mostly Paved"
 )
 Location.create(
                 title: "Point San Pedro Road to Paradise Drive",
                 desc: "description",
                 image_url: "url",
                 map_url: "url",
-                # start_lat: 37.,
-                # start_long: 122.,
-                # end_lat: 37.,
-                # end_long: 122.,
-                distance: 10,
-                elevation_up: 1,
-                elevation_down: 2,
-                terrain: "crapi"
+                start_lat: 37.9732,
+                start_long: 122.4837,
+                end_lat: 37.8728,
+                end_long: 122.4531,
+                distance: 13.4,
+                elevation_up: 223,
+                elevation_down: 217,
+                terrain: "Mostly Paved"
 )
 Location.create(
                 title: "Paradise Drive to Bothin Marsh",
                 desc: "description",
                 image_url: "url",
                 map_url: "url",
-                # start_lat: 37.,
-                # start_long: 122.,
-                # end_lat: 37.,
-                # end_long: 122.,
-                distance: 10,
-                elevation_up: 1,
-                elevation_down: 2,
-                terrain: "crapi"
+                start_lat: 37.8728,
+                start_long: 122.4531,
+                end_lat: 37.8880,
+                end_long: 122.5238,
+                distance: 6.2,
+                elevation_up: 167,
+                elevation_down: 171,
+                terrain: "Mostly Paved"
 )
 Location.create(
                 title: "Strawberry Drive to Golden Gate Bridge",
                 desc: "description",
                 image_url: "url",
                 map_url: "url",
-                # start_lat: 37.,
-                # start_long: 122.,
-                # end_lat: 37.8324,
-                # end_long: 122.4795,
-                distance: 10,
-                elevation_up: 1,
-                elevation_down: 2,
-                terrain: "crapi"
+                start_lat: 37.8880,
+                start_long: 122.5238,
+                end_lat: 37.8324,
+                end_long: 122.4795,
+                distance: 5.7,
+                elevation_up: 308,
+                elevation_down: 62,
+                terrain: "Mostly Paved"
 )
-
 Landmark.create(
                 title: "landmark",
                 desc: "cool place",
                 image_url: "url",
                 location_id: 1
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 2
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 3
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 5
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 6
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 7
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 8
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 9
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 10
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 11
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 12
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 13
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 14
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 15
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 16
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 17
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 18
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 19
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 20
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 21
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 22
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 23
+)
+Landmark.create(
+                title: "landmark",
+                desc: "cool place",
+                image_url: "url",
+                location_id: 24
 )
